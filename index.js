7553cf21c3f7fce889390a2e2706e35207ff8401
luckynana <Joynaew@gmail.com>
2023-02-01 10:45:48 -0500

    Progress check
    - added in small slideshow with 3 beers (css & js)

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 // const URL = ('https://api.punkapi.com/v2/beers')
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
 
 async function getBeers(beer_name) {
   const url = `https://api.punkapi.com/v2/beers/${beer_name}`
@@ -15,3 +45,4 @@ async function getBeers(beer_name) {
 }
 
 getBeers('')
+
