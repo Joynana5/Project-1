64c05a9470e99648265ad34e324ed21731d54b61
luckynana <Joynaew@gmail.com>
2023-02-01 11:52:25 -0500

    form attempt

--- index.js
+++ index.js
@@ -43,6 +43,38 @@ async function getBeers(beer_name) {
     console.log('');
   }
 }
-
 getBeers('')
 
+//setting up the 2 initial states to hold the search strings...
+const [searchString, setSearchString] = useState('')
+const [searchResult, setSearchResult] = useState([])
+
+// search by name function
+const search = document.querySelector('#search');
+// const todos = document.querySelectorAll('ul li');
+const notFound = document.querySelector('#notFound');
+
+search.addEventListener('keyup', filterFunctionality);
+function filterFunctionality(e) {
+  let searching = e.target.value.toLowerCase();
+
+  // Filters the todo
+  [...todos].forEach(todo => {
+    let todoContent = todo.textContent;
+    if (todoContent.toLowerCase().includes(searching)) {
+      todo.style.display = 'block';
+    } else {
+      todo.style.display = 'none';
+    }
+  });
+
+  // Displays No Todo Found
+  let result = [...todos].every(todo => {
+    return todo.style.display === 'none';
+  });
+
+  result === true
+    ? (notFound.style.display = 'block')
+    : (notFound.style.display = 'none');
+
+}
