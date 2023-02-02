8802ca4cce74b0a928a132d0d2be0bbf4835984c
luckynana <Joynaew@gmail.com>
2023-02-01 21:33:23 -0500

    console.log issues

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function showSlides(n) {
 
 // search by name function
 const search = document.querySelector('#search');
-// const todos = document.querySelectorAll('ul li');
+const todos = document.querySelectorAll('ul li');
 const notFound = document.querySelector('#notFound');
 
 // search.addEventListener('keyup', filterFunctionality);
@@ -67,14 +67,36 @@ function filterFunctionality(e) {
 
 document.getElementById('submit').addEventListener('click', (e) => {
   e.preventDefault()
-  let seachinput = document.getElementById('searchinput').value
+  let searchinput = document.getElementById('searchinput').value
+  let dropdown = document.getElementById('dropdown').value
+  let tempURL = ''
+
+  
+  switch (dropdown) {
+    case 'brandName': `https://api.punkapi.com/v2/beers?&beer_name=${searchinput}`
+      debugger
+      getData(tempURL)
+      break
+    case 'ABV': `https://api.punkapi.com/v2/beers?&abv_gt=${searchinput}`
+      debugger
+      getData(tempURL)
+      break
+    case 'EBC': `https://api.punkapi.com/v2/beers?&ebc_gt=${searchinput}`
+      getData(tempURL)
+      break
+
+    // default:
+    //  code block
+  }
 })
-let dropdown = document.getElementById('dropdown').value
-let tempURL = ''
-switch (dropdown) {
-  case 'brandName': `https://api.punkapi.com/v2/beers?&beer_name=${beer_name}`
-  case 'ingredients': ``
+async function getData(tempURL) {
+  let res = await fetch(`${tempURL}`)
+  let json = await res.json()
+  console.log(json)
+
 }
+
+
 
 
 // ocument.getElementById('submit').addEventListener('click', (e) => {
@@ -113,24 +135,24 @@ switch (dropdown) {
 // }
 
 
-async function getBrand(beer_name = 'Buzz') {
-  const url = `https://api.punkapi.com/v2/beers?&beer_name=${beer_name}`
+// async function getBrand(beer_name = 'Buzz') {
+//   const url = `https://api.punkapi.com/v2/beers?&beer_name=${beer_name}`
 
-  try {
-    const res = await fetch(url)
-    const json = await res.json()
-    console.log(json);
-    return json;
+//   try {
+//     const res = await fetch(url)
+//     const json = await res.json()
+//     console.log(json);
+//     return json;
 
-  } catch (error) {
-    console.log('');
-  }
-}
-form.addEventListener('submit', async e => {
-  e.preventDefault()
-  const beer = await getBrand(input.value)
-  console.log(beer)
-  // renderList(beer)
-})
+//   } catch (error) {
+//     console.log('');
+//   }
+// }
+// form.addEventListener('submit', async e => {
+//   e.preventDefault()
+//   const beer = await getBrand(input.value)
+//   console.log(beer)
+//   // renderList(beer)
+// })
 
-getBrand()
+// getBrand()
